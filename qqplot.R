640f9ab64d203d27947ebe7cc680dbf9236eb988
Paul Maley <maleyenator@gmail.com>
2016-11-06 16:11:38 +0100

    Add QQ example

---
+++ qqplot.R
@@ -0,0 +1,30 @@
+# Calculation
+
+# Sample size
+n <- 1000
+
+# Make n random numbers ~N(0,1) and order them
+xs <- sort(rnorm(n))
+# Or a t-distribution
+#xs <- sort(rt(n, df=5))
+# Uniform distribution [-3,3]
+#xs <- sort(runif(n,-3,3))
+
+# Define quartiles q by k/n+1 for K=1,n
+qs <- sapply(1:n, {function(x) x/(n+1)})
+
+# Determine corresponding z values
+zs <- qnorm(qs)
+
+# If we use the uniform distribution
+#zs <- qunif(qs,-3,3)
+
+# Plot
+plot(zs, xs)
+
+# Compare with qqnorm function
+layout(matrix(c(1,2),nrow=1))
+plot(zs,xs, xlab = "Theoretical", 
+     ylab = "Sample", main = "By hand")
+qqnorm(xs)
+lines(c(-3,3),c(-3,3))
